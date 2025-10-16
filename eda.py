f893b66efe27b645025f52fed6d1ea619b44bee7
abderrahmenyoussef <abderrahmen.youssef@polytechnicien.tn>
2025-10-16 10:12:37 +0100

    Premier commit : ajout de eda.py

---
+++ eda.py
@@ -0,0 +1,7 @@
+import numpy as np 
+import pandas as pd 
+#creer un array 2 dim 10lignes et 10cols 
+array_2d = np.random.randint(0,100,size=(10,3))
+#creer un dataframe a partir de cet array
+df = pd.DataFrame(array_2d)
+print(df.head())
