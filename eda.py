0e5a8c400e8289fa03809a548a206a3cddd7e2b2
abderrahmenyoussef <abderrahmen.youssef@polytechnicien.tn>
2025-10-18 23:11:16 +0100

    Modification de experiment-eda

--- eda.py
+++ eda.py
@@ -5,3 +5,5 @@ array_2d = np.random.randint(0,100,size=(10,3))
 #creer un dataframe a partir de cet array
 df = pd.DataFrame(array_2d)
 print(df.head())
+#afficher les infos du dataframe
+print(df.info())
